51d265fa7b3a8ebb1b85153d4e8e08c78a77b5f5
khoanguyenVDT <93902761+khoanguyenVDT@users.noreply.github.com>
2022-04-25 17:33:02 +0700

    init

---
+++ src/app.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserSchemaDocument } from './users.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AppService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly UserEntity: Model<UserSchemaDocument>,
+  ) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getUser(): any {
+    return this.UserEntity.find({});
+  }
+}
---
+++ src/users.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserSchemaDocument = User & Document;
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
